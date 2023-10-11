16e24e2947aa53b8a8eff6cba9286c1831e88771
Jay Miller <kjaymiller@gmail.com>
2023-10-10 23:41:35 -0400

    Enables checking of flat link and p values

--- src/render_engine_youtube_embed/plugins.py
+++ src/render_engine_youtube_embed/plugins.py
@@ -3,8 +3,19 @@ from render_engine.hookspecs import hook_impl
 from .youtube_embed import replace_youtube_links_with_embeds
 
 class YouTubeEmbed:
+    # @hook_impl
+    # def pre_build_collection(
+    #     ,
+    #     ) -> None:
+    #     if hasattr(page, "content"):
+    #         content = replace_youtube_links_with_embeds(page.content)    
+    #         page.content = content
+
     @hook_impl
-    def pre_build_collection_pages(page: "Page") -> None:
-        if hasattr(page, "content"):
+    def render_content(
+        page: "Page",
+        settings: {},
+    ):
+        if page._content:
             content = replace_youtube_links_with_embeds(page.content)    
-            page.content = content
+            page.content= content
